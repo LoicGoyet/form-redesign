68ff68c94eee71ac540498167e4ba09f91b0dfb4
Loïc Goyet <loic.goyet@resilience.care>
2023-09-21 23:24:41 +0200

    Handle promise in onSubmit event handler

--- src/views/LoginForm/index.tsx
+++ src/views/LoginForm/index.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { FormikHelpers } from 'formik'
 import * as React from 'react'
 import { PasswordWidget } from '@components/form/PasswordWidget'
 import { SubmitWidget } from '@components/form/SubmitWidget'
@@ -13,11 +14,51 @@ const initialValue = {
   password: '',
 }
 
+const serviceFn = async <Values extends object>(
+  values: Values
+): Promise<
+  | {
+      ok: true
+    }
+  | {
+      ok: false
+      errors: Partial<Record<keyof Values, string>>
+    }
+> => {
+  console.log(values)
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+
+  const isOk = Math.random() > 0.5
+
+  if (isOk) {
+    return { ok: true }
+  }
+
+  return {
+    ok: false,
+    errors: Object.keys(values).reduce(
+      (acc, key) => ({
+        ...acc,
+        [key]: 'test',
+      }),
+      {}
+    ),
+  }
+}
+
 const LoginForm = () => {
-  const handleSubmit = async (values: Login) => {
-    console.log(values)
-    await new Promise((resolve) => setTimeout(resolve, 2000))
-    return null
+  const handleSubmit = async (
+    values: Login,
+    formikHelper: FormikHelpers<Login>
+  ) => {
+    const response = await serviceFn(values)
+
+    if (response.ok) {
+      // Then we do whatever we want
+      return null
+    } else {
+      return formikHelper.setErrors(response.errors)
+    }
   }
 
   return (
