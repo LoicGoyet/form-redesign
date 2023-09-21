9cd7918a9c87e96830741371c0b5c418dde2f3dd
Loïc Goyet <loic.goyet@resilience.care>
2023-09-21 09:41:48 +0200

    Initial commit

---
+++ .storybook/preview.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react'
+import type { Preview } from '@storybook/react'
+import '../app/globals.scss'
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <div>
+        {/* 👇 Decorators in Storybook also accept a function. Replace <Story/> with Story() to enable it  */}
+        <Story />
+      </div>
+    ),
+  ],
+}
+
+export default preview
